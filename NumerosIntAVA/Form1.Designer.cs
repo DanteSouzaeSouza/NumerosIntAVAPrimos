5dd62b4d8ac3f8de45c9b401e90729bda81d4ac5
Dante Souza e Souza <lokinmodar@gmail.com>
2020-05-04 20:49:54 -0300

    Initial Commit

--- NumerosIntAVA/Form1.Designer.cs
+++ NumerosIntAVA/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NumerosIntAVA
 {
-    partial class Form1
+    partial class FrmNumeros
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lstNumeros = new System.Windows.Forms.ListBox();
+            this.btnFor = new System.Windows.Forms.Button();
+            this.btnWhile = new System.Windows.Forms.Button();
+            this.btnDo = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstNumeros
+            // 
+            this.lstNumeros.FormattingEnabled = true;
+            this.lstNumeros.Location = new System.Drawing.Point(12, 12);
+            this.lstNumeros.Name = "lstNumeros";
+            this.lstNumeros.Size = new System.Drawing.Size(209, 355);
+            this.lstNumeros.TabIndex = 0;
+            // 
+            // btnFor
+            // 
+            this.btnFor.Location = new System.Drawing.Point(304, 54);
+            this.btnFor.Name = "btnFor";
+            this.btnFor.Size = new System.Drawing.Size(75, 23);
+            this.btnFor.TabIndex = 1;
+            this.btnFor.Text = "For";
+            this.btnFor.UseVisualStyleBackColor = true;
+            this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
+            // 
+            // btnWhile
+            // 
+            this.btnWhile.Location = new System.Drawing.Point(304, 125);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(75, 23);
+            this.btnWhile.TabIndex = 2;
+            this.btnWhile.Text = "While";
+            this.btnWhile.UseVisualStyleBackColor = true;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
+            // 
+            // btnDo
+            // 
+            this.btnDo.Location = new System.Drawing.Point(304, 197);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(75, 23);
+            this.btnDo.TabIndex = 3;
+            this.btnDo.Text = "Do";
+            this.btnDo.UseVisualStyleBackColor = true;
+            this.btnDo.Click += new System.EventHandler(this.btnDo_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(304, 280);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // FrmNumeros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(454, 378);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnDo);
+            this.Controls.Add(this.btnWhile);
+            this.Controls.Add(this.btnFor);
+            this.Controls.Add(this.lstNumeros);
+            this.Name = "FrmNumeros";
+            this.Text = "Lista de números";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstNumeros;
+        private System.Windows.Forms.Button btnFor;
+        private System.Windows.Forms.Button btnWhile;
+        private System.Windows.Forms.Button btnDo;
+        private System.Windows.Forms.Button btnLimpar;
     }
 }
 
--- NumerosIntAVA/Form1.cs
+++ NumerosIntAVA/Form1.cs
@@ -10,11 +10,67 @@ using System.Windows.Forms;
 
 namespace NumerosIntAVA
 {
-    public partial class Form1 : Form
+    public partial class FrmNumeros : Form
     {
-        public Form1()
+        public FrmNumeros()
         {
             InitializeComponent();
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            // Limpando a lista
+            lstNumeros.Items.Clear();
+
+            // Iterar pelos números um a um
+            for ( int i = 0; i <= 100; i++ )
+            {
+                // adicionando um a um dentro da ListBox
+                lstNumeros.Items.Add(i);
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            // Limpando a lista
+            lstNumeros.Items.Clear();
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            // Limpando a lista
+            lstNumeros.Items.Clear();
+
+            // Criando a variável de controle
+            int i = 0;
+
+            while ( i <= 100)
+            {
+                // adicionando os elementos à lista
+                lstNumeros.Items.Add(i);
+                // incrementando a variável de controle
+                i++;
+            }
+
+
+
+        }
+
+        private void btnDo_Click(object sender, EventArgs e)
+        {
+            // Limpando a lista
+            lstNumeros.Items.Clear();
+
+            // Criando a variável de controle
+            int i = 0;
+
+            do
+            {
+                // adicionando os elementos à lista
+                lstNumeros.Items.Add(i);
+                // incrementando a variável de controle
+                i++;
+            } while (i <= 100);
         }
     }
 }
